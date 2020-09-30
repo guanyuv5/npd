61901244068d74f2b5688cc6fbeea3bc0c9456d7
guanyuding <guanyuding@tencent.com>
2020-10-01 00:56:17 +0800

    update config/plugin/check_file_nr.sh

--- config/plugin/check_file_nr.sh
+++ config/plugin/check_file_nr.sh
@@ -4,15 +4,13 @@ NONOK=1
 UNKNOWD=2
 percentage=0.8
 
-
 if [[ ! -f /proc/sys/fs/file-nr ]] ; then
         echo "/proc/sys/fs/file-nr is not exist"
         exit $UNKNOWD;
 fi
 read curr alloc limit < /proc/sys/fs/file-nr
-used=`echo "scale=5; (${curr} + ${alloc}) / ${limit} > ${percentage}" | bc`
+if ((  curr > limit * 8 /10 )); then
 
-if (( (${curr} + ${alloc}) > ${limit} * 8 /10 )); then
         echo "curr: ${curr}  alloc: ${alloc}  limit: ${limit}"
         exit $NONOK
 else
